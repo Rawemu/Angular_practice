2a45e46d76656e37b64516791172b515dfb9c799
Rawemu <rawemustarling@gmail.com>
2016-02-15 16:14:12 +0100

    Changes made to app.ts (array of names)

--- angular2-reddit-base/app.ts
+++ angular2-reddit-base/app.ts
@@ -1,20 +1,21 @@
 import { bootstrap } from "angular2/platform/browser";
 import { Component } from "angular2/core";
+import { NgFor } from "angular2/common";
 
 @Component({
     selector: "hello-world",
     template: `
-    <div>
-        Hello {{ name }}
-    </div>
+    <ul>
+        <li *ngFor="#name of names"> Hello {{ name }} </li>
+    </ul>
     `
 })
 
 class HelloWorld {
-    name: string;
+    names: string[];
 
     constructor() {
-        this.name = 'Dan';
+        this.names = ['Dan', 'Ari', 'Carlos'];
     }
 }
 
